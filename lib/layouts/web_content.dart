f6bf793e70152e38038369f06def3fa284ca0b98
CovenantJunior <covenanttope@gmail.com>
2024-01-01 19:18:00 +0100

    Handle progress

--- lib/layouts/web_content.dart
+++ lib/layouts/web_content.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/src/webview_controller.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebContent extends StatefulWidget {
-  const WebContent({super.key, required WebViewController controller});
+  const WebContent({super.key, required this.controller});
+  final WebViewController controller;
 
   @override
   State<WebContent> createState() => _WebContentState();
 }
 
 class _WebContentState extends State<WebContent> {
+  int progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            progress = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            progress = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            progress = 100;
+          });
+        },
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
